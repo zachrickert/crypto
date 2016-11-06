8078e0277294b1e1e7a7ed4551a0c1400825d3c4
zachrickert <zachrickert@gmail.com>
2016-11-06 04:28:45 -0800

    hex to base64 function added.

--- src/set1/hex_to_b64.py
+++ src/set1/hex_to_b64.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """Transforms hexidecimal string to a base64 string."""
+from binascii import unhexlify, b2a_base64
+
+HEXSTRING = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
 
-def hex_to_b64(hex):
+def hex_to_b64(hex_input):
     """Transform a hexidecimal string to a base64 string."""
+    return b2a_base64(unhexlify(hex_input)).rstrip('\n')
 
-    # Test to see if incoming is hexidecimal
-    try:
-        int(hex, 16)
-    except ValueError:
-        raise TypeError('Input string is not a hexidecimal.')
 
-    return hex
+if __name__ == '__main__':
+    print(hex_to_b64(HEXSTRING))
--- src/set1/test_hex_to_b64.py
+++ src/set1/test_hex_to_b64.py
@@ -6,10 +6,11 @@ from hex_to_b64 import hex_to_b64
 
 HEXSTRING = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 NOT_HEXSTRING = b"Not a hexistring"
-
+BASE64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 # -------------------Tests------------------------
 # [x] Accepts text if incoming is hexidecimal.
-# [] Type error if incoming is not hexidecimal.
+# [x] Type error if incoming is not hexidecimal.
+# [] Return correct value.
 
 
 def test_incoming_hexidecimal():
@@ -21,3 +22,8 @@ def test_incoming_not_hexidecimal_raises_error():
     """Accept text if incoming is hexidecimal."""
     with pytest.raises(TypeError):
         hex_to_b64(NOT_HEXSTRING)
+
+
+def test_outputs_correct_strip():
+    """Return correct value."""
+    assert hex_to_b64(HEXSTRING) == BASE64
